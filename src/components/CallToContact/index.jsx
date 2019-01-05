75bd6ef93d52d624cc5c08734119401067ad5a51
Ivan Santos <301291+pragmaticivan@users.noreply.github.com>
2019-01-05 11:48:40 -0800

    Updates contact e-mail

--- src/components/CallToContact/index.jsx
+++ src/components/CallToContact/index.jsx
@@ -3,7 +3,7 @@ import "./style.scss";
 
 const CallToContact = () => (
   <div className="CallToContact">
-    If you fancy a chat, drop me a line at
+    If you fancy a chat, drop me a line at{" "}
     <a href="mailto:hello@ivansantos.me">hello@ivansantos.me</a>
   </div>
 );
