86f572c98084a66de8dab4cbefe1bd9e51d35fd0
Ivan Santos <301291+pragmaticivan@users.noreply.github.com>
2023-07-17 20:33:39 -0500

    chore: add koala

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -34,6 +34,14 @@ export default class Doc extends Document {
           `,
             }}
           />
+
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+              !function(t){if(window.ko)return;window.ko=[],["identify","track","removeListeners","open","on","off","qualify","ready"].forEach(function(t){ko[t]=function(){var n=[].slice.call(arguments);return n.unshift(t),ko.push(n),ko}});var n=document.createElement("script");n.async=!0,n.setAttribute("src","https://cdn.getkoala.com/v1/pk_1ae2083eb48e188ba7146e0b7af3c40ed5ef/sdk.js"),(document.body || document.head).appendChild(n)}();
+          `,
+            }}
+          />
         </Head>
         <Main />
         <NextScript />
