416e5e556bde7340d792506c3fa8a392c9ab043d
Ivan Santos <301291+pragmaticivan@users.noreply.github.com>
2023-08-21 17:58:21 -0500

    feat: change color

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,7 +11,7 @@ function MyApp({ Component, pageProps, router }: AppProps) {
   const url = `https://ivansantos.me${router.asPath}`;
   return (
     <>
-      <NextNProgress color="#093747" />
+      <NextNProgress color="#127091" />
       <DefaultSeo
         titleTemplate="%s - Ivan Santos"
         openGraph={{
